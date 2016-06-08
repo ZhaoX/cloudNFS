484c69f1e681aafcb1bd50d03e4266efbf59449a
Xin Zhao <zhaoxina@gmail.com>
2016-06-08 10:06:34 +0800

    add config for org.dcache:nfs4j-core

---
+++ src/main/java/com/github/zhaox/cloudNFS/Application.java
@@ -0,0 +1,14 @@
+package com.github.zhaox.cloudNFS;
+
+/**
+ * @author zhaoxin
+ * @since 30.5.2016
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+    }
+
+}
